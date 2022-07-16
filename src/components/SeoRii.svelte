12701f4b4976f8a80273b03afbbe11b70d2ffc8f
Seohyun Lee <studioSeoRii@gmail.com>
2022-07-16 16:12:44 +0900

    Sync

--- src/components/SeoRii.svelte
+++ src/components/SeoRii.svelte
@@ -7,6 +7,9 @@
     let _iimg = {};
     let intv;
 
+    $: isMobile = innerWidth < 1024;
+    const filterLv = isMobile ? 8 : 14, bright = [0.98, 0.97, 0.89];
+
     $: {
         if (canvas) {
             canvas.width = innerWidth;
@@ -14,9 +17,8 @@
         }
     }
 
-    $: isMobile = innerWidth < 1024;
 
-    $: filt = Math.max(Math.min(12, Math.round((scrollY - 20) / (innerHeight - 200) * 12)), 0);
+    $: filt = Math.max(Math.min(filterLv, Math.round((scrollY - 20) / (innerHeight - 200) * filterLv)), 0);
 
 
     function snowdrop() {
@@ -39,8 +41,8 @@
             if (i.y - i.r > document.scrollingElement.scrollHeight) i.y = -i.r;
 
             const radgrad = ctx.createRadialGradient(i.x, i.y - scrollY * i.parallax, 0, i.x, i.y - scrollY * i.parallax, i.r);
-            radgrad.addColorStop(0, `rgba(${i.fill}, ${0.05 * i.o * ((36 - filt) / 36)})`);
-            radgrad.addColorStop(0.8, `rgba(${i.fill}, ${0.01 * i.o * ((36 - filt) / 36)})`);
+            radgrad.addColorStop(0, `rgba(${i.fill}, ${0.05 * i.o * (1 - filt / filterLv * 0.3)})`);
+            radgrad.addColorStop(0.8, `rgba(${i.fill}, ${0.01 * i.o * (1 - filt / filterLv * 0.3)})`);
             radgrad.addColorStop(1, `rgba(${i.fill}, 0)`);
             ctx.beginPath();
             ctx.arc(i.x, i.y - scrollY * i.parallax, i.r, 0, 2 * Math.PI);
@@ -65,32 +67,47 @@
             c2.width = 400;
             c2.height = 400;
             let _ctx = c2.getContext('2d');
-            for (let o of [0.98, 0.97, 0.89]) {
+            for (let o of bright) {
                 _iimg[o] = [];
-                for (let i = 0; i <= 12; i++) {
-                    _ctx.clearRect(0, 0, c2.width, c2.height);
-                    _ctx.filter = `blur(${i}px)`;
-                    _ctx.drawImage(img, 10, 10, c2.width - 20, c2.height - 20);
-                    await new Promise(resolve => setTimeout(resolve, 0));
-
-                    await new Promise((resolve) => {
-                        const img = new Image();
-                        img.onload = () => {
-                            _iimg[o].push(img);
-                            resolve();
+                _ctx.clearRect(0, 0, c2.width, c2.height);
+                _ctx.drawImage(img, 15, 15, c2.width - 30, c2.height - 30);
+                await new Promise((resolve) => {
+                    const img = new Image();
+                    img.onload = () => {
+                        for (let i = 0; i <= filterLv; i++) {
+                            _iimg[o][i] = (img);
                         }
-                        img.src = c2.toDataURL();
-                    })
-                }
-            }
+                        resolve();
+                    }
+                    img.src = c2.toDataURL();
+                })
 
-            c2.style.display = 'none';
+            }
+            (async () => {
+                for (let o of bright) {
+                    for (let i = 0; i <= filterLv; i++) {
+                        _ctx.clearRect(0, 0, c2.width, c2.height);
+                        _ctx.filter = `blur(${i}px)`;
+                        _ctx.drawImage(img, 15, 15, c2.width - 30, c2.height - 30);
+                        await new Promise((resolve) => {
+                            const img = new Image();
+                            img.onload = () => {
+                                _iimg[o][i] = (img);
+                                resolve();
+                            }
+                            img.src = c2.toDataURL();
+                        })
+                    }
+                }
+
+                c2.style.display = 'none';
+            })().then();
 
             let t = 0;
             ctx = canvas.getContext("2d");
-            for (let i = 0; i < 12; i++) {
+            for (let i = 0; i < Math.sqrt(document.scrollingElement.scrollHeight) / 4; i++) {
                 if (t > 10000) break;
-                const w = isMobile ? (Math.random() * 200 + 100) : (Math.random() * 300 + 400),
+                const w = isMobile ? (Math.random() * 250 + 100) : (Math.random() * 300 + 400),
                     h = w / img.width * img.height,
                     x = Math.random() * (innerWidth - w + 400) - 200,
                     y = Math.random() * (document.scrollingElement.scrollHeight + 400) - 200
@@ -110,12 +127,12 @@
 
                 snow.push({
                     x, y, w, h,
-                    o: [0.98, 0.97, 0.89][Math.floor(Math.random() * 3)],
+                    o: bright[Math.floor(Math.random() * bright.length)],
                     speed: Math.random() * 0.2 + 0.02,
                     parallax: Math.random() * 0.4 + 0.4
                 });
             }
-            for (let i = 0; i < Math.sqrt(innerHeight * innerWidth) / 5; i++) {
+            for (let i = 0; i < Math.sqrt(document.scrollingElement.scrollHeight * innerWidth) / 8; i++) {
                 const r = Math.random() * 50 + 200,
                     x = Math.random() * (innerWidth - r + 400) - 200,
                     y = Math.random() * (document.scrollingElement.scrollHeight + 400) - 200
@@ -123,8 +140,8 @@
                     x, y, r,
                     o: Math.random(),
                     d: Math.random() * 0.001 - 0.002,
-                    speedx: Math.random() + 0.02,
-                    speedy: Math.random() + 0.02,
+                    speedx: Math.random() * 2 - 1,
+                    speedy: Math.random() * 2 - 1,
                     fill: ['72, 117, 237', '72, 117, 237', '66, 66, 66'][Math.floor(Math.random() * 3)],
                     parallax: Math.random() * 0.2 + 0.7
                 });
@@ -138,7 +155,7 @@
                     i.speedx = Math.max(i.speedx, -1);
                     i.speedy = Math.max(i.speedy, -1);
                 }
-            }, 1000)
+            }, 200)
             snowdrop();
         };
     });
