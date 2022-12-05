bd2f73f6dbd878c18cab94738b1c1d2f2d26463c
Seohyun Lee <studioseorii@gmail.com>
2022-12-05 23:52:08 +0900

    Sync

--- gh-pages.js
+++ gh-pages.js
@@ -7,7 +7,7 @@ publish(
         repo: 'https://github.com/Seo-Rii/Seo-Rii.github.io.git', // Update to point to your repository
         user: {
             name: 'Seohyun Lee', // update to use your name
-            email: 'studioSeoRii@gmail.com' // Update to use your email
+            email: 'me@seorii.page' // Update to use your email
         },
         dotfiles: true
     },
