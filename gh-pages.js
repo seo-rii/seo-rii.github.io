6230ef7ee5df686e4949f906b6c2811723d24583
seo-rii <me@seorii.page>
2024-06-24 18:12:51 +0900

    ci: fix deploy

--- gh-pages.js
+++ gh-pages.js
@@ -1,17 +1,19 @@
-import { publish } from 'gh-pages';
+import {publish} from 'gh-pages';
 
 publish(
     'build', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/Seo-Rii/Seo-Rii.github.io.git', // Update to point to your repository
+        repo: 'https://github.com/seo-rii/seo-rii.github.io.git', // Update to point to your repository
         user: {
             name: 'Seohyun Lee', // update to use your name
             email: 'me@seorii.page' // Update to use your email
         },
-        dotfiles: true
+        dotfiles: true,
+        cname: 'seorii.page',
+        nojekyll: true,
     },
-    () => {
-        console.log('Deploy Complete!');
+    (e) => {
+        console.log(e)
     }
-);
+).then(() => console.log('Done.'));
